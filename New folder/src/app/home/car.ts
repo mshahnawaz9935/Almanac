872aea078e357361c02d67fec0915462ce31d2d6
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-04-19 12:11:50 +0100

    update

--- New folder/src/app/home/car.ts
+++
@@ -1,9 +0,0 @@
-export class Car {
-  constructor(
-    public id: number,
-    public name: string,
-    public type: string,
-    public ncb :string,
-    public alterEgo?: string
-  ) {  }
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { FooterComponent } from './footer/footer.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LayoutModule} from 'ng2-flex-layout';
 import { SearchComponent } from './search/search.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SearchComponent } from './search/search.component';
     MenuBarComponent,
     FooterComponent,
   HomeFormComponent,
-  SearchComponent
+  SearchComponent,
+  PostsComponent
   ],
   entryComponents: [
     AppComponent,
