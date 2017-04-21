1b998644ff545efa985fc2bb0dd52938038cea20
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-04-21 16:58:20 +0100

    changes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -20,7 +20,7 @@ results;username='';password='';
     onEnter(value: string) {
 
     this.value = value;
-    this.http.get('http://localhost:3000/api/search?id='+ this.value)
+    this.http.get('http://angular2ap.azurewebsites.net/api/search?id='+ this.value)
         .map((res: Response) => res.json()).subscribe((dataFromServer) => {
           this.data = dataFromServer;
           this.getdata(dataFromServer);
