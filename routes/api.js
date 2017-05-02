c00513cde153f10f93a12b7b8eb99fb8f57d080c
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-05-02 12:19:52 +0100

    change

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,14 @@ var MongoClient = require('mongodb').MongoClient , format = require('util').form
 var User     = require('../models/user');
 var mongoose   = require('mongoose');
 var session = require('express-session');
-//mongoose.connect('mongodb://remotemongodb:J3gcFVlTzb4KznFQ8Rbsz7V7cEROONHgSQMXkyI8wswQ41afGnkEvkn1iYmT01ktjvCH1FLOSYiaQi0t893rNw==@remotemongodb.documents.azure.com:10250/?ssl=true/details');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+try{
+mongoose.connect('mongodb://remotemongodb:J3gcFVlTzb4KznFQ8Rbsz7V7cEROONHgSQMXkyI8wswQ41afGnkEvkn1iYmT01ktjvCH1FLOSYiaQi0t893rNw==@remotemongodb.documents.azure.com:10250/?ssl=true/details');
+
+}
+catch(err){
+    mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
 
 
 /* GET api listing. */
