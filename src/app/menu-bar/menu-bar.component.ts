e6aba7785835392683dc7e758eb0cff6a66b331d
Shahnawaz Alam <mshahnawaz9935@gmail.com>
2017-05-13 17:30:35 +0100

    update

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http ,Response} from '@angular/http'; 
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-menu-bar',
@@ -6,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent implements OnInit {
+authenticated1 = false;
+user = '';
+   constructor(private http:Http) {
+       this.http.get('https://angular2ap.azurewebsites.net/onenote/checklogin')
+        .map((res: Response) => res.json()).subscribe((dataFromServer) => {
+          console.log('Login status is ' + dataFromServer );
+          if(dataFromServer == 'No Login')
+          this.authenticated1 = false;
+          else
+          { this.authenticated1 =true;
+          console.log(this.authenticated1);
 
-  constructor() { }
+          this.http.get('https://angular2ap.azurewebsites.net/onenote/aboutme')
+              .map((res: Response) => res.json()).subscribe((dataFromServer) => {
+                console.log('Login status is ' + dataFromServer );
+                this.user = dataFromServer;
+              });
+          }
+        });
+   }
 
   ngOnInit() {
   }
