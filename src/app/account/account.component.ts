96adbb67a5f92aff4d29514f4e83976126df6875
Shahnawaz Alam <mshahnawaz9935@gmail.com>
2017-05-13 17:19:25 +0100

    account component update

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http ,Response} from '@angular/http'; 
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-account',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+authenticated1 = false;
+user = '';
+  constructor(private http:Http) {
+      this.http.get('https://angular2ap.azurewebsites.net/onenote/checklogin')
+        .map((res: Response) => res.json()).subscribe((dataFromServer) => {
+          console.log('Login status is ' + dataFromServer );
+          if(dataFromServer == 'No Login')
+          this.authenticated1 = false;
+          else
+          { this.authenticated1 =true;
+          console.log(this.authenticated1);
 
-  constructor() { }
+          this.http.get('https://angular2ap.azurewebsites.net/onenote/aboutme')
+              .map((res: Response) => res.json()).subscribe((dataFromServer) => {
+                console.log('Login status is ' + dataFromServer );
+                this.user = dataFromServer;
+              });
+          }
+        });
+   }
 
   ngOnInit() {
   }
 
+ 
 }
