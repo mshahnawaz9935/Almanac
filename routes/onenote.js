124cdc28c75ff19bf92f6b7d08f693ac5ffa6a40
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-06-12 16:34:03 +0100

    update

--- routes/onenote.js
+++ routes/onenote.js
@@ -500,6 +500,8 @@ function writespecific(token,topic,chapter ,callback)
         },
         }, function (error, response, body){
             favourites = response.body;
+            if(favourites.sections != undefined)
+            {
             console.log(favourites.sections.section.length);
             for(var i=0; i< favourites.sections.section.length; i++) 
             {  
@@ -516,6 +518,14 @@ function writespecific(token,topic,chapter ,callback)
                     url = url+ "<p><img src=" + "\"" + favourites.sections.section[i].images.image[j].url + "\"" + "/></p>";
                     }
                 }
+            }
+            else 
+            {
+                  favourites= { title: 'Page does not exists: Error 404' };
+                  url = url + "<p> Contact your School or Teacher</p>";
+                  console.log('I come here when i get error' , favourites.title);
+
+            }
                 callback(url);
             
         });
