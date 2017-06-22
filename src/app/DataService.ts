1957ad46b504b977a57d31b5a7d82051c80da99d
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-06-22 12:14:39 +0100

    update

--- src/app/DataService.ts
+++ src/app/DataService.ts
@@ -16,12 +16,12 @@ slider1='';
 slider2='';
     constructor(private http: Http) {
 
-        this.http.get('https://angular2ap.azurewebsites.net/api/token')
+        this.http.get('http://localhost:3000/api/token')
               .map((res: Response) => res.json()).subscribe((dataFromServer) => 
                dataFromServer
               );
 
-    this.http.get('https://angular2ap.azurewebsites.net/onenote/checklogin')
+    this.http.get('http://localhost:3000/onenote/checklogin')
         .map((res: Response) => res.json()).subscribe((dataFromServer) => {
           console.log('Data Service Login status is ' + dataFromServer );
           if(dataFromServer == 'No Login')
@@ -39,7 +39,7 @@ slider2='';
     getInstances() {
 
          // ...using get request
-         return this.http.get('https://angular2ap.azurewebsites.net/api/instances')
+         return this.http.get('http://localhost:3000/api/instances')
                         // ...and calling .json() on the response to return data
                          .map((res:Response) => res.json())
                          //...errors if any
