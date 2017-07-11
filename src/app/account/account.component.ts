d84c46ca3585979b792b0553ae29a5b5b5e6e914
medeirol <medeirol@scss.tcd.ie>
2017-07-11 11:58:25 +0100

    Merge branch 'master' of https://github.com/mshahnawaz9935/Almanac

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Http ,Response} from '@angular/http'; 
 import 'rxjs/add/operator/map';
+import { Router } from '@angular/router';
+import { DataService } from '../DataService';
 
 @Component({
   selector: 'app-account',
@@ -10,7 +12,12 @@ import 'rxjs/add/operator/map';
   export class AccountComponent implements OnInit {
   authenticated1 = false;
   user = '';
-  constructor(private http:Http) {}
+  constructor(private http:Http , private router:Router , private DataService:DataService) {
+        if(this.DataService.authenticated1 == true)
+    {
+      this.router.navigate(['/modules']);
+    }
+  }
 
   ngOnInit() {}
 
@@ -23,5 +30,6 @@ import 'rxjs/add/operator/map';
     {
       alert('Functionality no longer supported. Login using Office 365');
     }
+    
  
 }
