0f016a7ea61c21ebab96b31920bebcd126cb4146
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-08-23 12:24:05 +0100

    update

--- server.js
+++ server.js
@@ -37,12 +37,6 @@ app.use('/api', api);
 app.use('/note', note);
 app.use('/onenote', onenote);
 
-
-
-
-
-
-
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
