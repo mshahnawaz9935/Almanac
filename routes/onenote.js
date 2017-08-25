1d4bf786cef09503962bd7bd5e05239d581d7026
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-08-25 17:08:28 +0100

    update

--- routes/onenote.js
+++ routes/onenote.js
@@ -1529,6 +1529,7 @@ var counter =0;
 
         createNewPage2(token, data , false , function(response)
         {
+            console.log(response);
             callback(JSON.parse(response));
         });
          });
