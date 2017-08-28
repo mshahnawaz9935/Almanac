1b4d69d48114cee8b3153e61e2dee3295b0790dc
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-08-28 16:43:26 +0100

    update

--- routes/onenote.js
+++ routes/onenote.js
@@ -1128,11 +1128,11 @@ var counter =0;
             // {
               mode.sections.forEach(function(section)
               {
-                  if(chapter == 'Common Gases' && counter > 6)
-                  {
-                    console.log('BREAKING');
-                  return true;
-                  }
+                  // if(chapter == 'Common Gases' && counter > 8)
+                  // {
+                  //   console.log('BREAKING');
+                  // return true;
+                  // }
                   
                   url = url + "<h1>" + section.title + "</h1>";
                     url = url + "<h2>" +  section.text.text.substring(9, section.text.text.length-3 ) + "</h2>";
