f7e63058e2ebfbe6aeb3d197767f01e9d00f8af6
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-08-28 15:36:12 +0100

    update

--- routes/onenote.js
+++ routes/onenote.js
@@ -1128,7 +1128,7 @@ var counter =0;
             // {
               mode.sections.forEach(function(section)
               {
-                  if(chapter == 'Common Gases' && counter > 3)
+                  if(chapter == 'Common Gases' && counter > 5)
                   {
                     console.log('BREAKING');
                   return true;
