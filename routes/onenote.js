3d4ea78fbb4ed2dcc21fe75df50254c26ca78f85
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-08-29 10:36:17 +0100

    update

--- routes/onenote.js
+++ routes/onenote.js
@@ -1128,7 +1128,7 @@ var counter =0;
             // {
               mode.sections.some(function(section)
               {
-                  if(chapter == 'The States of Matter' && counter > 3)
+                  if(chapter == 'The States of Matter' && counter > 2)
                   {
                     console.log('BREAKING' , counter);
                   return true;
