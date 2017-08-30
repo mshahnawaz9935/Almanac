5bf528f950f737f2a52d325800fd01bdc0a76d06
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-08-30 11:37:36 +0100

    update

--- routes/onenote.js
+++ routes/onenote.js
@@ -1173,7 +1173,7 @@ var counter =0;
                                  {
                                   
                                   console.log('File size is', ((obj.data.toString().length-814)/1.37)/1000);
-                                  if(((obj.data.toString().length-814)/1.37)/1000 < 250)
+                                  if(((obj.data.toString().length-814)/1.37)/1000 < 200)
                                   {
                                      console.log('matched');
                                       console.log('File url is' ,fileurl );
